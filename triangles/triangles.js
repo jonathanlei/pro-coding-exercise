ae4290760f1ff4685c978b05b06a8fb8b2ff0756
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-11-10 17:38:42 -0800

    implemented triangles.js

--- triangles/triangles.js
+++ triangles/triangles.js
@@ -1,39 +1,50 @@
 /* Is size a valid integer 2-50? */
 
 function validateEdge(size) {
-  return (Number.isInteger(size) &&
-      size >= 2 &&
-      size <= 50);
+  return (size % 1 === 0 &&
+    size >= 2 &&
+    size <= 50);
 }
-
-
+/* takes 2 sides and calculates area of triangle */
+function calculateArea(sideA, sideB) {
+  return (sideA * sideB) / 2
+}
+/* takes 2 sides and calculates hypotenuse of triangle */
+function calculateHypotenuse(sideA, sideB) {
+  return Math.floor(Math.sqrt(sideA ** 2 + sideB ** 2));
+}
 /* Handle UI: get form data & update HTML */
-
 function processForm(evt) {
   evt.preventDefault();
-  let sideA = +document.getElementById("side-a").value;
-  let sideB = +document.getElementById("side-b").value;
-
-  let sideAisValid = validateEdge(a);
-  let sideBisValid = validateEdge(b);
-
+  let sideA = parseInt(document.getElementById("side-a").value);
+  let sideB = parseInt(document.getElementById("side-b").value);
+  let [sideAisValid, sideBisValid] = validateTriangle(sideA, sideB);
+  setMessages(sideAisValid, sideBisValid, sideA, sideB);
+}
+/* Takes 2 sides and returns if triangle is valid. */
+function validateTriangle(sideA, sideB) {
+  let sideAisValid = validateEdge(sideA);
+  let sideBisValid = validateEdge(sideB);
+  return [sideAisValid, sideBisValid];
+}
+/* Called by processForm, sets messages in DOM*/
+function setMessages(sideAisValid, sideBisValid, sideA, sideB) {
   let aMsg = sideAisValid ? "" : "Invalid!";
   let bMsg = sideBisValid ? "" : "Invalid!";
-
-  let msg;
-
-  if (sideAisValid && sideBisValid) {
-    let area = sideA * sideB / 2;
-    let hypot = Math.floor(Math.sqrt(a * a + b * b));
-    let msg = `Hypotenuse is ${hypot} and area is ${area}.`;
-    if (area > 50) {
-      msg += ` That's a really big triangle!`;
-    }
-  } else {
-    msg = "";
-  }
-
+  let calcMessage = "";
   document.getElementById("a-msg").innerHTML = aMsg;
   document.getElementById("b-msg").innerHTML = bMsg;
-  document.getElementById("msg").innerHTML = msg;
+
+  if (sideAisValid && sideBisValid) {
+    let area = calculateArea(sideA, sideB);
+    let hypot = calculateHypotenuse(sideA, sideB);
+    calcMessage = `Hypotenuse is ${hypot} and area is ${area}.`;
+    if (area > 50) {
+      calcMessage += ` That's a really big triangle!`;
+    }
+    // console.log(`Area: ${area}, hypot:${hypot}, msg:${msg}`)
+  } else {
+    calcMessage = "";
+  }
+  document.getElementById("msg").innerHTML = calcMessage;
 }
